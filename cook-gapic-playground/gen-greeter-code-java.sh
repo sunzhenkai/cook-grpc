d2bedb9dd816a30322de9fc6c1b53d2c3d617193
sunzhenkai <zhenkai.sun@qq.com>
2021-05-26 18:01:06 +0800

    config

--- cook-gapic-playground/gen-greeter-code-java.sh
+++ cook-gapic-playground/gen-greeter-code-java.sh
@@ -1,11 +1,12 @@
 OUTPUT=java-code-gen
 [ -e $OUTPUT ] && rm -r $OUTPUT
 mkdir $OUTPUT
-java -cp bin/gapic-generator-2.11.1-fatjar.jar \
+cd $OUTPUT
+java -cp ../bin/gapic-generator-2.11.1-fatjar.jar \
     com.google.api.codegen.GeneratorMain LEGACY_GAPIC_AND_PACKAGE \
-    --descriptor_set=greeter.proto.pb \
-    --service_yaml=greeter-service.yaml \
-    --gapic_yaml=greeter-gapic-config.yaml \
+    --descriptor_set=../greeter.proto.pb \
+    --service_yaml=../greeter-service.yaml \
+    --gapic_yaml=../greeter-gapic-config.yaml \
+    --package_yaml2=../greeter-meta-config.yaml \
     --language=java \
-    --package_yaml2=greeter-meta-config.yaml \
-    --o=$OUTPUT
+    --o=.
