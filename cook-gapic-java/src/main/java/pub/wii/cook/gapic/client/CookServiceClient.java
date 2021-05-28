096c791072c81bfe7b09912054c9f1ca1775d0e6
sunzhenkai <zhenkai.sun@qq.com>
2021-05-28 15:42:29 +0800

    add java simple

---
+++ cook-gapic-java/src/main/java/pub/wii/cook/gapic/client/CookServiceClient.java
@@ -0,0 +1,30 @@
+package pub.wii.cook.gapic.client;
+
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import wii.pub.gapic.cook.v1.CookServiceGrpc;
+import wii.pub.gapic.cook.v1.PingRequest;
+
+public class CookServiceClient {
+
+    private final CookServiceGrpc.CookServiceBlockingStub bs;
+
+    public CookServiceClient(Channel channel) {
+        this.bs = CookServiceGrpc.newBlockingStub(channel);
+    }
+
+    public String ping() {
+        return this.bs.ping(PingRequest.newBuilder().setMsg("ping").build()).getRes();
+    }
+
+    public static void main(String[] args) {
+        String target = "localhost:9000";
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                .usePlaintext()
+                .build();
+        CookServiceClient client = new CookServiceClient(channel);
+        String res = client.ping();
+        System.out.println(res);
+    }
+}
---
+++ cook-gapic-java/src/main/java/pub/wii/cook/gapic/service/CookServiceImpl.java
@@ -0,0 +1,43 @@
+package pub.wii.cook.gapic.service;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import wii.pub.gapic.cook.v1.CookServiceGrpc;
+import wii.pub.gapic.cook.v1.PingResponse;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class CookServiceImpl extends CookServiceGrpc.CookServiceImplBase {
+    private Server server;
+
+    @Override
+    public void ping(wii.pub.gapic.cook.v1.PingRequest request, io.grpc.stub.StreamObserver<wii.pub.gapic.cook.v1.PingResponse> responseObserver) {
+        System.out.println("received message: " + request.getMsg());
+        PingResponse response = PingResponse.newBuilder().setRes(request.getMsg() + ": pong").build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    public void start() throws IOException {
+        int port = 9000;
+        server = ServerBuilder.forPort(port)
+                .addService(this)
+                .build()
+                .start();
+    }
+
+    public void stop() throws InterruptedException {
+        server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+
+    public void block() throws InterruptedException {
+        server.awaitTermination();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        CookServiceImpl impl = new CookServiceImpl();
+        impl.start();
+        impl.block();
+    }
+}
