bc3b4f9ac47274e39eca1e1205715c9c43f239d4
sunzhenkai <zhenkai.sun@qq.com>
2021-09-19 16:32:09 +0800

    mod

--- cook-gapic-java/src/main/java/pub/wii/cook/gapic/client/DefaultCookServiceGapicClient.java
+++ cook-gapic-java/src/main/java/pub/wii/cook/gapic/client/DefaultCookServiceGapicClient.java
@@ -13,7 +13,7 @@ import pub.wii.cook.v1.PingRequest;
 import pub.wii.cook.v1.PingResponse;
 import pub.wii.cook.v1.stub.CookServiceStubSettings;
 
-public class SimpleCookServiceGapicClient {
+public class DefaultCookServiceGapicClient {
     public static void main(String[] args) throws Exception {
         String target = "127.0.0.1:9000";
 
@@ -22,15 +22,14 @@ public class SimpleCookServiceGapicClient {
                 .usePlaintext()
                 .build();
 
-        TransportChannel tc = GrpcTransportChannel.create(channel);
-        TransportChannelProvider tcp = FixedTransportChannelProvider.create(tc);
         CookServiceSettings cs = CookServiceSettings.create(CookServiceStubSettings.newBuilder()
                 .setCredentialsProvider(new NoCredentialsProvider())
-                .setTransportChannelProvider(tcp)
                 .build());
         CookServiceClient client = CookServiceClient.create(cs);
-        PingResponse response = client.ping(PingRequest.newBuilder().setMsg("ping").build());
-        String res = response.getRes();
-        System.out.println(res);
+        System.out.println(client.getSettings());
+        System.out.println(client.getSettings().getStubSettings());
+//        PingResponse response = client.ping(PingRequest.newBuilder().setMsg("ping").build());
+//        String res = response.getRes();
+//        System.out.println(res);
     }
 }
